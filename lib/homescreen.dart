5a725c7542fdd6ee3f9cdcd94ea856f5d8e2af34
Rezerbiw12 <teerawut1212z@hotmail.com>
2020-01-17 22:01:42 +0700

    get test api

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'profilescreen.dart';
+import 'package:http/http.dart' as http;
 import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  get() async {
+    var url = "https://jsonplaceholder.typicode.com/posts/";
+    var res = await http.get(url);
+    var map = json.decode(utf8.decode(res.bodyBytes));
+    print("${map}");
+    setState(() {
+      data = map;
+    });
+  }
+
   int _current = 0;
   List imageList = [
     "https://scontent.fbkk9-2.fna.fbcdn.net/v/t1.0-9/77279153_2448141005474457_5998275016909651968_o.jpg?_nc_cat=109&_nc_ohc=qOZ-N9eaK_sAQkxlD0Iapnvk4SHEn_j05ksMDHrFxHHh44Zi_Krird_8A&_nc_ht=scontent.fbkk9-2.fna&oh=5fd45bbb91cd92de7bbbcfe6afd3024a&oe=5EDB0F3F",
@@ -16,35 +28,48 @@ class _HomeScreenState extends State<HomeScreen> {
     "https://scontent.fbkk12-1.fna.fbcdn.net/v/t1.0-9/76916299_2448140908807800_5702951278930296832_o.jpg?_nc_cat=101&_nc_ohc=JW_RVU6iLKMAQnJj3h7FZVxLbW96kJ_ikXEue_wkQO4YAkw1ClA_ZxznQ&_nc_ht=scontent.fbkk12-1.fna&oh=83887c60a6cf02d96a49bbd5f2347a1e&oe=5E9B5A67",
     "https://scontent.fbkk12-2.fna.fbcdn.net/v/t1.0-9/75252982_2448140828807808_8951400233632268288_o.jpg?_nc_cat=105&_nc_ohc=gZJWr5UgiC0AQkkG-Ivr93heeHevXL_8Svj88Fp52R1LShzVU9SrGhY2g&_nc_ht=scontent.fbkk12-2.fna&oh=f7115a2a4e00d60a9aa194ccd589bead&oe=5EA42228"
   ];
+  var data;
+  @override
+  void initState() {
+    get();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeScreen'),
-        centerTitle: true,
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: IconButton(
-              icon: const Icon(
-                Icons.account_circle,
-                size: 30,
+        appBar: AppBar(
+          title: Text('HomeScreen'),
+          centerTitle: true,
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.account_circle,
+                  size: 30,
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                },
               ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ProfileScreen()));
-              },
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+            )
+          ],
+        ),
+        body: Column(
           children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "My Gallery",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
             CarouselSlider(
-                height: 400,
+                aspectRatio: 16 / 9,
+                autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                height: 200,
                 enlargeCenterPage: true,
                 autoPlay: true,
                 initialPage: 0,
@@ -57,17 +82,46 @@ class _HomeScreenState extends State<HomeScreen> {
                   return Builder(
                     builder: (BuildContext context) {
                       return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 10.0),
-                        decoration: BoxDecoration(color: Colors.green),
-                        child: Image.network(imgUrl, fit: BoxFit.fill),
-                      );
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 10.0),
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(imgUrl, fit: BoxFit.fill),
+                          ));
                     },
                   );
-                }).toList())
+                }).toList()),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: data == null ? 0 : data.length,
+                itemBuilder: (context, index) {
+                  return test(context, index);
+                },
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget test(BuildContext context, int index) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Container(
+          child: Card(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.album, size: 50),
+              title: Text('Heart Shaker'),
+              subtitle: Text(data[index]['title']),
+            ),
           ],
         ),
-      ),
+      )),
     );
   }
 }
